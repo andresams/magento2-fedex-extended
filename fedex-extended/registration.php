3d7db3ba2a49c6688935eeef94cbf7b9ff104258
Andresa Martins <contact@andresa.dev>
2020-04-20 01:36:06 -0300

    Added module registration and license

---
+++ fedex-extended/registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Module Registration
+ *
+ * @category   Prestafy
+ * @package    Prestafy_FedexExtended
+ * @author     Andresa Martins <contact@andresa.dev>
+ * @copyright  Copyright (c) 2020 Prestafy eCommerce Solutions (https://www.prestafy.com.br)
+ * @license    https://prestafy.com.br/licenses/free-for-use PRESTAFY FREE FOR USE
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Prestafy_FedexExtended',
+    __DIR__);
